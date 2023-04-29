5983e3156a0a5718a3b7d883bd2574e29a79a196
DESKTOP-EA4FO0M\Horace <pifi77@hotmail.fr>
2023-04-29 16:59:04 +0200

    Musci pre then loop

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -7,6 +7,10 @@ public class MusicManager : MonoBehaviour
     public AudioSource musicIntro;
     public AudioSource musicLoop;
 
+    public float crossFadeDuration = 0.5f;
+    public float crossFadeOffset = 0.5f;
+    public float volume = 1;
+
     private void Start()
     {
         StartCoroutine(MusiqueAndLoop());
@@ -14,15 +18,29 @@ public class MusicManager : MonoBehaviour
 
     IEnumerator MusiqueAndLoop()
     {
-        if(musicIntro.clip != null)
+        musicIntro.volume = volume;
+        yield return 0;
+        if (musicIntro.clip != null)
         {
             musicIntro.Play();
-            yield return new WaitForSeconds(musicIntro.clip.length);
+            Debug.Log("musicIntro.clip.length "+ -crossFadeDuration + " crossFadeDuration "+ musicIntro.clip.length );
+            yield return new WaitForSeconds(musicIntro.clip.length - crossFadeDuration - crossFadeOffset);
         }
         else
         {
             yield return 0;
         }
+
+        musicLoop.volume = 0;
         musicLoop.Play();
+        float lerp = 0;
+        while (lerp < 1)
+        {
+            lerp += Time.deltaTime / crossFadeDuration;
+            musicIntro.volume = volume * (1-lerp);
+            musicLoop.volume = volume * lerp;
+            yield return 0;
+        }
+        musicLoop.volume = volume;
     }
 }
